a2570ed545a28c54b725dfb4d5cb2747e0729e86
Yeray <bniquelau@gmail.com>
2015-07-12 15:51:14 +0200

    Añadidos cambios en la clase Java

--- src/cursos/Java.java
+++ src/cursos/Java.java
@@ -1,13 +1,22 @@
 package cursos;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class Java extends JFrame{
+public class Java extends JFrame implements ListSelectionListener{
 
+	// Creamos un array con los valores que va a contener la lista.
 	String [] elementos = {"Introducción","Variables","Condicionales","Bucles", "Break y Continue", 
 			"Switch y Case", "Arrays y Matrices", "Métodos, clases y objetos", 
 			"Herencia y polimorfismo", "Tratamiento de cadenas de texto", "Entorno gráfico 1: a, b, c", };
+	
+	// Y lo añadimos a la lista para poder seleccionarlos.
 	JList list = new JList(elementos);
+	
+	JPanel contenido = new JPanel();
+	JLabel lblPrueba = new JLabel("Hola que tal");
 	
 	public Java(){
 		super("Java");
@@ -18,9 +27,28 @@ public class Java extends JFrame{
 		list.setBounds(12, 12, 187, 570);
 		getContentPane().add(list);
 		
-		JLabel lblProgramacionJava = new JLabel("Programación Java");
-		lblProgramacionJava.setBounds(215, -16, 522, 72);
-		getContentPane().add(lblProgramacionJava);
+		contenido.setBounds(209, 12, 803, 716);
+		getContentPane().add(contenido);
 		setVisible(true);
+		
+		list.addListSelectionListener(this);
 	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		if(list.getSelectedIndex() == 0){
+			contenido.removeAll();
+			contenido.add(lblPrueba);
+			contenido.updateUI();
+			contenido.repaint();
+			repaint();
+		}
+		else if(list.getSelectedIndex() == 1){
+			contenido.removeAll();
+			contenido.updateUI();
+			contenido.repaint();
+			repaint();
+		}
+	}
+
 }
